77d0f14beeda0a06d847604776ca0e29e14e3188
0xtunan-star <0xtunan@gmail.com>
2026-08-08 02:22:05 +0800

    Update usb_mouse.c

--- usb_mouse.c
+++ usb_mouse.c
@@ -3,11 +3,13 @@
 #include <string.h>
 
 
-#define REPORT_ID_MOUSE    1
-#define REPORT_ID_KEYBOARD 2
+#define REPORT_ID_MOUSE       1
+#define REPORT_ID_KEYBOARD    2
+
 
 
 // HID Report Descriptor
+
 uint8_t const desc_hid_report[] =
 {
     TUD_HID_REPORT_DESC_MOUSE(
@@ -22,15 +24,16 @@ uint8_t const desc_hid_report[] =
 
 
 #define CONFIG_TOTAL_LEN \
-    (TUD_CONFIG_DESC_LEN + 2*TUD_HID_DESC_LEN)
+    (TUD_CONFIG_DESC_LEN + TUD_HID_DESC_LEN)
 
 
 
 uint8_t const desc_configuration[] =
 {
+
     TUD_CONFIG_DESCRIPTOR(
         1,
-        2,
+        1,
         0,
         CONFIG_TOTAL_LEN,
         TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP,
@@ -38,59 +41,56 @@ uint8_t const desc_configuration[] =
     ),
 
 
-    // Mouse Interface
-
     TUD_HID_DESCRIPTOR(
         0,
         0,
-        HID_ITF_PROTOCOL_MOUSE,
+        HID_ITF_PROTOCOL_NONE,
         sizeof(desc_hid_report),
         0x81,
-        16,
-        10
-    ),
-
-
-    // Keyboard Interface
-
-    TUD_HID_DESCRIPTOR(
-        1,
-        0,
-        HID_ITF_PROTOCOL_KEYBOARD,
-        sizeof(desc_hid_report),
-        0x82,
-        16,
+        64,
         10
     )
+
 };
 
 
 
 tusb_desc_device_t const desc_device =
 {
+
     .bLength = sizeof(tusb_desc_device_t),
+
     .bDescriptorType = TUSB_DESC_DEVICE,
 
     .bcdUSB = 0x0200,
+
 
     .bDeviceClass = 0x00,
     .bDeviceSubClass = 0x00,
     .bDeviceProtocol = 0x00,
 
+
     .bMaxPacketSize0 = CFG_TUD_ENDPOINT0_SIZE,
 
+
+    // Logitech VID/PID
 
     .idVendor = 0x046D,
     .idProduct = 0xC539,
 
+
     .bcdDevice = 0x0100,
 
-    .iManufacturer = 0x01,
-    .iProduct = 0x02,
-    .iSerialNumber = 0x03,
+
+    .iManufacturer = 1,
+    .iProduct = 2,
+    .iSerialNumber = 3,
+
 
     .bNumConfigurations = 1
+
 };
+
 
 
 
@@ -101,11 +101,74 @@ uint8_t const * tud_descriptor_device_cb(void)
 
 
 
+
 uint8_t const * tud_descriptor_configuration_cb(uint8_t index)
 {
     (void)index;
+
     return desc_configuration;
 }
+
+
+
+
+uint16_t const * tud_descriptor_string_cb(
+        uint8_t index,
+        uint16_t langid)
+{
+
+    (void)langid;
+
+
+    static uint16_t str[32];
+
+
+    if(index == 0)
+    {
+        str[0] = 0x0304;
+        str[1] = 0x0409;
+
+        return str;
+    }
+
+
+
+    const char *s;
+
+
+    if(index == 1)
+        s = "Logitech";
+
+    else if(index == 2)
+        s = "Pico Combo";
+
+    else if(index == 3)
+        s = "123456";
+
+    else
+        return NULL;
+
+
+
+    size_t len = strlen(s);
+
+
+    for(size_t i=0;i<len;i++)
+    {
+        str[1+i]=s[i];
+    }
+
+
+
+    str[0]=(TUSB_DESC_STRING<<8)|(2*len+2);
+
+
+
+    return str;
+
+}
+
+
 
 
 
@@ -119,67 +182,109 @@ uint8_t const * tud_hid_descriptor_report_cb(uint8_t instance)
 
 
 
+
 void tud_hid_set_report_cb(
-    uint8_t instance,
-    uint8_t report_id,
-    hid_report_type_t type,
-    uint8_t const *buffer,
-    uint16_t bufsize)
+        uint8_t instance,
+        uint8_t report_id,
+        hid_report_type_t type,
+        uint8_t const *buffer,
+        uint16_t bufsize)
 {
 
+    (void)instance;
+    (void)report_id;
+    (void)type;
+    (void)buffer;
+    (void)bufsize;
+
 }
+
+
 
 
 
 uint16_t tud_hid_get_report_cb(
-    uint8_t instance,
-    uint8_t report_id,
-    hid_report_type_t type,
-    uint8_t *buffer,
-    uint16_t reqlen)
+        uint8_t instance,
+        uint8_t report_id,
+        hid_report_type_t type,
+        uint8_t *buffer,
+        uint16_t reqlen)
 {
+
     return 0;
+
 }
 
 
 
 
-void send_mouse(int8_t dx,int8_t dy)
-{
-    uint8_t report[5];
 
-    report[0]=0;
-    report[1]=dx;
-    report[2]=dy;
-    report[3]=0;
+static void send_mouse(int8_t dx,int8_t dy)
+{
+
+    uint8_t report[6];
+
+
+    report[0]=REPORT_ID_MOUSE;
+
+    report[1]=0;
+
+    report[2]=dx;
+
+    report[3]=dy;
+
     report[4]=0;
 
+    report[5]=0;
+
+
 
     tud_hid_report(
-        REPORT_ID_MOUSE,
+        0,
         report,
         sizeof(report)
     );
+
 }
 
 
 
-void send_keyboard(uint8_t modifier,uint8_t key)
+
+
+static void send_keyboard(uint8_t modifier,uint8_t key)
 {
 
-    uint8_t report[8]={0};
+    uint8_t report[9];
 
 
-    report[0]=modifier;
-    report[2]=key;
+    report[0]=REPORT_ID_KEYBOARD;
+
+
+    report[1]=modifier;
+
+    report[2]=0;
+
+    report[3]=key;
+
+
+    report[4]=0;
+    report[5]=0;
+    report[6]=0;
+    report[7]=0;
+    report[8]=0;
+
 
 
     tud_hid_report(
-        REPORT_ID_KEYBOARD,
+        0,
         report,
         sizeof(report)
     );
+
 }
+
+
+
 
 
 
@@ -192,8 +297,11 @@ int main()
     tusb_init();
 
 
-
     int step=0;
+
+
+    uint32_t last_key_time=0;
+
 
 
     while(1)
@@ -206,8 +314,6 @@ int main()
         if(tud_hid_ready())
         {
 
-
-            // 鼠标移动
 
             int8_t dx =
                 3*(step%20-10)/10;
@@ -225,26 +331,25 @@ int main()
 
 
 
-            // 测试键盘 A
-
-            static uint32_t last=0;
-
             uint32_t now =
                 to_ms_since_boot(
                     get_absolute_time()
                 );
 
 
-            if(now-last>5000)
+
+            if(now-last_key_time > 5000)
             {
+
 
                 send_keyboard(
                     0,
-                    HID_KEY_A
+                    HID_KEY_SPACE
                 );
 
 
                 sleep_ms(50);
+
 
 
                 send_keyboard(
@@ -253,12 +358,13 @@ int main()
                 );
 
 
-                last=now;
+                last_key_time=now;
 
             }
 
 
         }
+
 
 
         sleep_ms(20);
